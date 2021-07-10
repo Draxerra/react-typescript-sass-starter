299e84f8a40858cea1eeaec337dabb7c7e954dcd
MichaelTugby <michael.tugby5@gmail.com>
2021-07-10 04:02:47 +0100

    Add basic e2e tests

--- src/e2e/home.e2e.ts
+++ src/e2e/home.e2e.ts
@@ -1,5 +1,18 @@
-describe("Home", () => {
-  it("works", () => {
-    expect(true).to.equal(true);
+describe("Counter", () => {
+  it("should start with a count of 0", () => {
+    cy.visit("/");
+    cy.contains("Current Count: 0").should("exist");
+  });
+
+  it("should increment by 1", () => {
+    cy.visit("/");
+    cy.contains("Increment").click();
+    cy.contains("Current Count: 1").should("exist");
+  });
+
+  it("should decrement by 1", () => {
+    cy.visit("/");
+    cy.contains("Decrement").click();
+    cy.contains("Current Count: -1").should("exist");
   });
 });
